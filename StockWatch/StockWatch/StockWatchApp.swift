7972d596677cafd8c27905cd1d77327db71e721a
HyoTaek <kht1790@gmail.com>
2026-02-22 22:38:00 +0900

    [Feat] 프로젝트 생성

---
+++ StockWatch/StockWatch/StockWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockWatchApp.swift
+//  StockWatch
+//
+//  Created by HyoTaek on 2/22/26.
+//
+
+import SwiftUI
+
+@main
+struct StockWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
