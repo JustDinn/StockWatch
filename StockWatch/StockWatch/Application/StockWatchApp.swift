4231c56c28cf3fea6ee1bdec9cadbb91b5f108fe
HyoTaek <kht1790@gmail.com>
2026-02-22 22:49:18 +0900

    [Feat] HomeView 추가 및 프로젝트 디렉토리 정리

--- StockWatch/StockWatch/Application/StockWatchApp.swift
+++ StockWatch/StockWatch/Application/StockWatchApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct StockWatchApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
---
+++ StockWatch/StockWatch/Presentation/Home/View/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  StockWatch
+//
+//  Created by HyoTaek on 2/22/26.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
