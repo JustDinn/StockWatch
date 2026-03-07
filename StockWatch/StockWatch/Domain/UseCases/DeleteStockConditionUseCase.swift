39442005c54be3baa3be6597e9adce94562f3cfa
HyoTaek <kht1790@gmail.com>
2026-03-07 23:53:01 +0900

    [Feat] 종목 전략 조건 삭제 UseCase 구현

--- StockWatch/StockWatch/Domain/UseCases/DeleteStockConditionUseCase.swift
+++ StockWatch/StockWatch/Domain/UseCases/DeleteStockConditionUseCase.swift
@@ -7,12 +7,18 @@
 final class DeleteStockConditionUseCase: DeleteStockConditionUseCaseProtocol {
 
     private let repository: StockConditionRepositoryProtocol
+    private let alertRepository: AlertRegistrationRepositoryProtocol
 
-    init(repository: StockConditionRepositoryProtocol) {
+    init(
+        repository: StockConditionRepositoryProtocol,
+        alertRepository: AlertRegistrationRepositoryProtocol
+    ) {
         self.repository = repository
+        self.alertRepository = alertRepository
     }
 
     func execute(id: String) async throws {
+        try? await alertRepository.unregister(conditionId: id)
         try await repository.delete(id: id)
     }
 }
