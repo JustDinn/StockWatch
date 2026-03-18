ff3c84e3fba0985c4737bbda24b605fb7f973c05
HyoTaek <kht1790@gmail.com>
2026-03-18 22:36:08 +0900

    [UI] 설정 탭 생성

---
+++ StockWatch/StockWatch/Presentation/Settings/View/SettingsView.swift
@@ -0,0 +1,19 @@
+//
+//  SettingsView.swift
+//  StockWatch
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        NavigationStack {
+            Text("")
+                .navigationTitle("설정")
+        }
+    }
+}
+
+#Preview {
+    SettingsView()
+}
